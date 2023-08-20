725b3ec35facd45a9aee30504e21e1625c1cc4e0
exxxpm <127376127+exxxpm@users.noreply.github.com>
2023-08-20 15:14:21 +0400

    Edit SwiperBreadcrumbs

--- widgets/SwiperBreadcrumbs.php
+++ widgets/SwiperBreadcrumbs.php
@@ -12,119 +12,53 @@ use yii\base\Widget;
 use yii\helpers\Html;
 use yii\widgets\Breadcrumbs;
 
-class SwiperBreadcrumbs extends Widget{
-    public array $rout_array = [
-        'Object' => [
-            'object_id' => '/web/site/object/',
-        ],
-        'Home' => [
-            'object_id' => '/web/site/object/',
-            'home_id' => '/web/home/index/',
-        ],
-        'Entrance' => [
-            'object_id' => '/web/site/object/',
-            'home_id' => '/web/home/index/',
-            'entrance_id' => '/web/entrance/index/',
-        ],
-        'Floor' => [
-            'object_id' => '/web/site/object/',
-            'home_id' => '/web/home/index/',
-            'entrance_id' => '/web/entrance/index/',
-            'floor_id' => '/web/floor/index/',
-        ],
-        'Flat' => [
-            'object_id' => '/web/site/object/',
-            'home_id' => '/web/home/index/',
-            'entrance_id' => '/web/entrance/index/',
-            'floor_id' => '/web/floor/index/',
-            'flat_id' => '/web/flat/index/',
-        ],
-        'Room' => [
-            'object_id' => '/web/site/object/',
-            'home_id' => '/web/home/index/',
-            'entrance_id' => '/web/entrance/index/',
-            'floor_id' => '/web/floor/index/',
-            'flat_id' => '/web/flat/index/',
-            'room_id' => '/web/room/index/'
-        ]
-    ];
 
+class SwiperBreadcrumbs extends Widget{
     public $model_name;
     public $model_id;
     public array $links = [];
 
-    public function run()
-    {
-        switch ($this->model_name) {
+    public function generateLinks($model, $model_id) {
+        switch ($model) {
             case 'Object':
-                $object = Objects::findOne($this->model_id);
-                $this->links = [
-                    ['label' => $object->name]
-                ];
-                break;
+                $object = Objects::findOne($model_id);
+                return $this->generateLinksWithLabel($object->name, $this->model_name == 'Object' ? null : '/web/site/object/' . $object->id);
             case 'Home':
-                $home = Home::findOne($this->model_id);
-                $object = Objects::findOne($home->object_id);
-                $this->links = [
-                    ['label' => $object->name, 'url' => '/web/site/object/' . $object->id],
-                    ['label' => $home->name]
-                ];
-                break;
+                $home = Home::findOne($model_id);
+                $object_links = $this->generateLinks('Object', $home->object_id);
+                return $this->generateLinksWithLabel($home->name, $this->model_name == 'Home' ? null : '/web/home/index/' . $home->id, $object_links);
             case 'Entrance':
-                $entrance = Entrance::findOne($this->model_id);
-                $home = Home::findOne($entrance->home_id);
-                $object = Objects::findOne($entrance->object_id);
-                $this->links = [
-                    ['label' => $object->name, 'url' => '/web/site/object/' . $object->id],
-                    ['label' => $home->name, 'url' => '/web/home/index/' . $home->id],
-                    ['label' => $entrance->name]
-                ];
-                break;
+                $entrance = Entrance::findOne($model_id);
+                $home_links = $this->generateLinks('Home', $entrance->home_id);
+                return $this->generateLinksWithLabel($entrance->name, $this->model_name == 'Entrance' ? null : '/web/entrance/index/' . $entrance->id, $home_links);
             case 'Floor':
-                $floor = Floor::findOne($this->model_id);
-                $entrance = Entrance::findOne($floor->entrance_id);
-                $home = Home::findOne($floor->home_id);
-                $object = Objects::findOne($floor->object_id);
-                $this->links = [
-                    ['label' => $object->name, 'url' => '/web/site/object/' . $object->id],
-                    ['label' => $home->name, 'url' => '/web/home/index/' . $home->id],
-                    ['label' => $entrance->name, 'url' => '/web/entrance/index/' . $entrance->id],
-                    ['label' => $floor->name]];
-                break;
+                $floor = Floor::findOne($model_id);
+                $entrance_links = $this->generateLinks('Entrance', $floor->entrance_id);
+                return $this->generateLinksWithLabel($floor->name, $this->model_name == 'Floor' ? null : '/web/floor/index/' . $floor->id, $entrance_links);
             case 'Flat':
-                $flat = Flat::findOne($this->model_id);
-                $floor = Floor::findOne($flat->floor_id);
-                $entrance = Entrance::findOne($flat->entrance_id);
-                $home = Home::findOne($flat->home_id);
-                $object = Objects::findOne($flat->object_id);
-                $this->links = [
-                    ['label' => $object->name, 'url' => '/web/site/object/' . $object->id],
-                    ['label' => $home->name, 'url' => '/web/home/index/' . $home->id],
-                    ['label' => $entrance->name, 'url' => '/web/entrance/index/' . $entrance->id],
-                    ['label' => $floor->name, 'url' => '/web/floor/index/' . $floor->id],
-                    ['label' => $flat->name]
-                ];
-                break;
+                $flat = Flat::findOne($model_id);
+                $floor_links = $this->generateLinks('Floor', $flat->floor_id);
+                return $this->generateLinksWithLabel($flat->name, $this->model_name == 'Flat' ? null : '/web/flat/index/' . $flat->id, $floor_links);
             case 'Room':
-                $room = Room::findOne($this->model_id);
-                $flat = Flat::findOne($room->flat_id);
-                $floor = Floor::findOne($room->floor_id);
-                $entrance = Entrance::findOne($room->entrance_id);
-                $home = Home::findOne($room->home_id);
-                $object = Objects::findOne($room->object_id);
-                $this->links = [
-                    ['label' => $object->name, 'url' => '/web/site/object/' . $object->id],
-                    ['label' => $home->name, 'url' => '/web/home/index/' . $home->id],
-                    ['label' => $entrance->name, 'url' => '/web/entrance/index/' . $entrance->id],
-                    ['label' => $floor->name, 'url' => '/web/floor/index/' . $floor->id],
-                    ['label' => $flat->name, 'url' => '/web/flat/index/' . $flat->id],
-                    ['label' => $room->name]
-                ];
-                break;
+                $room = Room::findOne($model_id);
+                $flat_links = $this->generateLinks('Flat', $room->flat_id);
+                return $this->generateLinksWithLabel($room->name, null, $flat_links);
+            default:
+                return [];
         }
+    }
 
+    private function generateLinksWithLabel($label, $url = null, $additionalLinks = []) {
+        $link = ['label' => $label];
+        if ($url !== null) {
+            $link['url'] = $url;
+        }
+        return array_merge($additionalLinks, [$link]);
+    }
+
+    public function run(){
         $params = [
-            'links' => $this->links,
+            'links' => $this->generateLinks($this->model_name, $this->model_id),
             'itemTemplate' => "<li class='breadcrumb-item swiper-slide' itemprop='itemListElement' itemscope itemtype='http://schema.org/ListItem'>{link}<meta itemprop='position' content='{position}' /></li>\n",
             'activeItemTemplate' => "<li class='breadcrumb-item active swiper-slide' itemprop='itemListElement' itemscope itemtype='http://schema.org/ListItem'>{link}<meta itemprop='position' content='{position}' /></li>\n",
             'options' => [
